6f91897dcadaecba4f6f12b557131b17274c6741
Eric Aguerreberry <62189322+rediberry@users.noreply.github.com>
2020-03-23 03:46:46 -0300

    Add files via upload

---
+++ ejercicio_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fechastring;
+            DateTime fecha_hoy = DateTime.Now;
+            TimeSpan diferencia_fechas;           
+            DateTime fecha;
+            int dias;        
+
+           
+            do
+            {
+                Console.WriteLine("Ingrese una fecha válida");
+                fechastring = Console.ReadLine();                
+                
+
+            } while (DateTime.TryParse(fechastring, out fecha) != true);
+            diferencia_fechas = fecha_hoy - fecha;
+            dias = diferencia_fechas.Days;
+
+            Console.WriteLine("La diferencia entre la fecha ingresada y el día de hoy es " + dias + " día/s");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ ejercicio_9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuario = "Suyai";
+            string usuario2;
+            do
+            {
+                Console.WriteLine("Ingrese su usuario");
+                usuario2 = Console.ReadLine();
+                if (usuario2 != usuario)
+                {
+                    Console.WriteLine("No te conozco");
+                }
+                
+            } while (usuario2 != usuario);
+
+            Console.WriteLine("¡Hola, " + usuario2 +"!");
+            Console.ReadKey();
+
+        }
+    }
+}
